a1840330f8a51b7026848526a44f32465d941abb
liuguoqiang <liugq01@rd.netease.com>
2023-03-05 01:27:45 +0800

    mod: 增加示例

--- CoroutineExample/CoroutineExample/ViewController.swift
+++ CoroutineExample/CoroutineExample/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SwiftCoroutine
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var stackView: UIStackView!
     
     override func viewDidLoad() {
@@ -37,7 +37,26 @@ class ViewController: UIViewController {
     }
     
     func btn_1_didClicked() {
-        print(#function)
+        DispatchQueue.main.startCoroutine {
+            
+            let (data, response, error) = try Coroutine.await { callback in
+                let url = URL.init(string: "https://github.com/belozierov/SwiftCoroutine")!
+                URLSession.shared.dataTask(with: url, completionHandler: callback).resume()
+            }
+            let (data_1, response_1, error_1) = try Coroutine.await { callback in
+                let url = URL.init(string: "https://github.com/belozierov?tab=repositories")!
+                URLSession.shared.dataTask(with: url, completionHandler: callback).resume()
+            }
+            let (data_2, response_2, error_2) = try Coroutine.await { callback in
+                let url = URL.init(string: "https://github.com/belozierov/SwiftCoroutine")!
+                URLSession.shared.dataTask(with: url, completionHandler: callback).resume()
+            }
+            let (data_3, response_3, error_3) = try Coroutine.await { callback in
+                let url = URL.init(string: "https://www.baidu.com")!
+                URLSession.shared.dataTask(with: url, completionHandler: callback).resume()
+            }
+            print(response)
+        }
     }
     
     func btn_2_didClicked() {
